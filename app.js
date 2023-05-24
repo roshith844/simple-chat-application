bc84b789e1b5ab42772ae19e05b44b7ff635d8e9
ROSHITH P <roshith844@gmail.com>
2023-05-24 18:41:32 +0530

    chore: reformat

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-const { response } = require('express')
 const express = require('express')
-const { listen } = require('socket.io')
 const app = express()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
@@ -21,12 +19,12 @@ io.sockets.on('connection', (socket) => {
           console.log('disconnected : %s socket connected', connections.length)
      })
      socket.on('send message', (data, room) => {
-          console.log("room"+ room)
+          console.log("room" + room)
           if (room === '') {
                io.sockets.emit('new message', { msg: data })
-          }else{
+          } else {
                io.to(room).emit('new message', { msg: data })
           }
      })
 })
-console.log('server listenin')
+console.log('server listening')
