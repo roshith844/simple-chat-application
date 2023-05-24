49cca0893f31554099e1881415e4b06a1bdbd76c
ROSHITH P <roshith844@gmail.com>
2023-05-24 18:40:25 +0530

    feat: add room

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const { response } = require('express')
 const express = require('express')
-const {listen} = require('socket.io')
+const { listen } = require('socket.io')
 const app = express()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
@@ -8,21 +8,25 @@ users = []
 connections = []
 server.listen(3000)
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
      res.sendFile(__dirname + '/index.html')
 })
 
-io.sockets.on('connection', (socket)=>{
+io.sockets.on('connection', (socket) => {
      // when client connects to server
      connections.push(socket)
      console.log('connected: %s socket connected', connections.length) // current connections
-     socket.on('disconnect', (data)=>{
+     socket.on('disconnect', (data) => {
           connections.splice(connections.indexOf(socket), 1) // delete plug details
           console.log('disconnected : %s socket connected', connections.length)
      })
-     socket.on('send message', (data)=>{
-          console.log(data)
-          io.sockets.emit('new message', {msg: data })
+     socket.on('send message', (data, room) => {
+          console.log("room"+ room)
+          if (room === '') {
+               io.sockets.emit('new message', { msg: data })
+          }else{
+               io.to(room).emit('new message', { msg: data })
+          }
      })
 })
 console.log('server listenin')
